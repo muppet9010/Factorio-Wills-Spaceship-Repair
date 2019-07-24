2717b500109ae71687607f2c7e4014dc38bf9cd6
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-07-24 13:50:40 +0000

    remove temporary Audit Orders fixit script

--- scripts/order-audit.lua
+++ scripts/order-audit.lua
@@ -12,55 +12,6 @@ function OrderAudit.OnLoad()
     SlotStates = global.StaticData.Orders.slotStates
     Commands.Register("wills_spaceship_repair-write_order_data", {"api-description.wills_spaceship_repair-write_order_data"}, OrderAudit.WriteOutTableCommand, false)
     Events.RegisterHandler("Orders.OrderSlotUpdated", "OrderAudit.LogOrder", OrderAudit.LogOrder)
-
-    Commands.Register("fixit", nil, OrderAudit.FixIt, false)
-end
-
-function OrderAudit.FixIt()
-    local currentOrderId = 0
-    for i, auditOrder in ipairs(global.Orders.orderAuditTable) do
-        if auditOrder.endedTime == "" then
-            global.Orders.orderAuditTable[i] = "bob"
-        else
-            currentOrderId = currentOrderId + 1
-            auditOrder.orderId = currentOrderId
-            auditOrder.auditIndex = nil
-        end
-    end
-
-    for i = 1, #global.Orders.orderAuditTable do
-        if global.Orders.orderAuditTable[i] ~= nil and global.Orders.orderAuditTable[i] == "bob" then
-            table.remove(global.Orders.orderAuditTable, i)
-        end
-    end
-    for i = 1, #global.Orders.orderAuditTable do
-        if global.Orders.orderAuditTable[i] ~= nil and global.Orders.orderAuditTable[i] == "bob" then
-            table.remove(global.Orders.orderAuditTable, i)
-        end
-    end
-
-    for orderSlotIndex, auditIndex in ipairs(global.Orders.orderAuditMap) do
-        local order = global.Orders.orderSlots[orderSlotIndex]
-        order.orderId = auditIndex
-    end
-
-    local newOrderAuditTable = {}
-    for _, auditOrder in ipairs(global.Orders.orderAuditTable) do
-        newOrderAuditTable[auditOrder.orderId] = {
-            orderId = auditOrder.orderId,
-            orderSlot = auditOrder.orderSlot,
-            item = auditOrder.item,
-            itemCountNeeded = auditOrder.itemCountNeeded,
-            itemCountDone = auditOrder.itemCountNeeded,
-            startTime = auditOrder.startTime,
-            endedTime = auditOrder.endedTime,
-            stateName = auditOrder.stateName
-        }
-    end
-    global.Orders.orderAuditTable = newOrderAuditTable
-
-    global.Orders.currentOrderId = currentOrderId
-    global.Orders.orderAuditMap = nil
 end
 
 function OrderAudit.LogOrder(event)
